78742ed82150355a93808fd78053bd067a1c6d0f
Brady Messer <30396538+bradymesser@users.noreply.github.com>
2021-10-25 11:34:30 -0400

    start stop

--- commands/stop.js
+++ commands/stop.js
@@ -2,7 +2,12 @@ module.exports = {
     name: '!stop',
     description: 'Stop',
     execute(msg, args) {
-
+        const channel = msg.member.voiceChannel;
+        if (channel) {
+            channel.voiceChannel.end();
+        } else {
+            msg.reply('Join a channel numb nuts');
+        }
         msg.reply('Stopping');
     },
 };
