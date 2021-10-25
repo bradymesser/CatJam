983b5ba6c32ff9615612d6f25931f4bdc7da7e6b
Brady Messer <30396538+bradymesser@users.noreply.github.com>
2021-10-25 10:48:30 -0400

    Add search

--- commands/play.js
+++ commands/play.js
@@ -1,32 +1,45 @@
 const ytdl = require('ytdl-core');
+const search = require('youtube-search');
+require('dotenv').config();
 
 module.exports = {
     name: '!play',
     description: 'Play',
     async execute(msg, args) {
-
-        const tempUrl = args[0];
+        let tempUrl = args[0];
         const channel = msg.member.voiceChannel;
         const isValid = ytdl.validateURL(tempUrl);
+
+        // If no argument is supplied
         if (!tempUrl) {
             msg.reply('Invalid request');
             return;
         }
+        // If the argument is not a valid link, try to search yt for it
         if (!isValid) {
-            msg.reply('Invalid URL');
-            return;
+            tempUrl = null;
+            search(args[0], { maxResults: 1, key: process.env.YT_KEY }, (err, results) => {
+                if (err) {
+                    msg.reply(err);
+                }
+                if (results.length) {
+                    tempUrl = results[0].link;
+                } else {
+                    tempUrl = null;
+                    msg.reply('No results');
+                }
+            })
         }
-        if (channel) {
+        if (channel && tempUrl) {
             const connection = await channel.join();
             const stream = ytdl(tempUrl, { filter: 'audioonly' });
             const dispatcher = connection.playStream(stream);
             msg.reply(`Playing ${tempUrl}`);
             dispatcher.on('end', () => {
-                channel.leave();
+                connection.destroy();
             })
         } else {
             msg.reply('Join a channel numb nuts');
         }
-        // connection.play()
     },
 };
