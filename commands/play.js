3c4fe9a944d15150f831550b09ca039185e36691
Brady Messer <30396538+bradymesser@users.noreply.github.com>
2021-10-25 01:10:40 -0400

    Testing play

--- commands/play.js
+++ commands/play.js
@@ -1,9 +1,28 @@
+const ytdl = require('ytdl-core');
+const tempUrl = 'https://www.youtube.com/watch?v=R_uS0aT0bG8'
 module.exports = {
     name: '!play',
     description: 'Play',
     execute(msg, args) {
         msg.reply('Playing');
-        console.log(msg.member.voiceChannel)
-        msg.member.voiceChannel.join();
+        const channel = msg.member.voiceChannel;
+        const isValid = ytdl.validateURL(tempUrl);
+        if (!isValid) {
+            msg.reply('Invalid URL');
+            return;
+        }
+        if (channel) {
+            const connection = await channel.join();
+            const stream = ytdl(tempUrl, { filter: 'audioonly' });
+            const dispatcher = connection.playStream(stream);
+
+            dispatcher.on('end', () => {
+                channel.leave();
+
+            })
+        } else {
+            msg.reply('Join a channel numb nuts');
+        }
+        // connection.play()
     },
 };
